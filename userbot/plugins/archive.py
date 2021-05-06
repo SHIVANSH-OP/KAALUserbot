ca50f42398cd48d14cb2c7d35375db09e723cb49
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-05-06 08:28:02 +0530

    Update archive.py

--- userbot/plugins/archive.py
+++ userbot/plugins/archive.py
@@ -19,7 +19,7 @@ from telethon.tl.types import DocumentAttributeVideo
 
 from userbot import CMD_HELP
 from userbot.Config import Config
-from W2HBOT.utils import admin_cmd, progress
+from kaalBOT.utils import admin_cmd, progress
 from userbot.cmdhelp import CmdHelp
 
 thumb_image_path = Config.TMP_DOWNLOAD_DIRECTORY + "/thumb_image.jpg"
@@ -59,7 +59,7 @@ async def _(event):
     await borg.send_file(
         event.chat_id,
         directory_name + ".zip",
-        caption="Zipped By W2HBOT",
+        caption="Zipped By kaalBOT",
         force_document=True,
         allow_cache=False,
         reply_to=event.message.id,
@@ -106,7 +106,7 @@ async def _(event):
             await borg.send_file(
                 event.chat_id,
                 directory_name + ".rar",
-                caption="rarred By W2HBOT",
+                caption="rarred By kaalBOT",
                 force_document=True,
                 allow_cache=False,
                 reply_to=event.message.id,
