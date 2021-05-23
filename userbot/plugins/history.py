81978bcd0ed81a93b554f759cdd77c4131947f0c
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-05-23 22:38:45 +0530

    Update history.py

--- userbot/plugins/history.py
+++ userbot/plugins/history.py
@@ -1,25 +1,25 @@
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.account import UpdateNotifySettingsRequest
-from W2HBOT import bot, CmdHelp
-from W2HBOT.utils import admin_cmd, edit_or_reply as eor, sudo_cmd
+from kaalBOT import bot, CmdHelp
+from kaalBOT.utils import admin_cmd, edit_or_reply as eor, sudo_cmd
 
-@W2HBOT.on(admin_cmd(pattern="history ?(.*)"))
-@W2HBOT.on(sudo_cmd(pattern="history ?(.*)", allow_sudo=True))
-async def _(W2Hevent):
-    if W2Hevent.fwd_from:
+@kaalBOT.on(admin_cmd(pattern="history ?(.*)"))
+@kaalBOT.on(sudo_cmd(pattern="history ?(.*)", allow_sudo=True))
+async def _(kaalevent):
+    if kaalevent.fwd_from:
         return 
-    if not W2Hevent.reply_to_msg_id:
-       await eor(W2Hevent, "`Please Reply To A User To Get This Module Work`")
+    if not kaalevent.reply_to_msg_id:
+       await eor(kaalevent, "`Please Reply To A User To Get This Module Work`")
        return
     reply_message = await W2Hevent.get_reply_message() 
     chat = "Sangmatainfo_bot"
     victim = reply_message.sender.id
     if reply_message.sender.bot:
-       await eor(W2Hevent, "Need actual users. Not Bots")
+       await eor(kaalevent, "Need actual users. Not Bots")
        return
-    await eor(W2Hevent, "Checking...")
-    async with W2Hevent.client.conversation(chat) as conv:
+    await eor(Wkaalevent, "Checking...")
+    async with kaalevent.client.conversation(chat) as conv:
           try:     
               response1 = conv.wait_event(events.NewMessage(incoming=True,from_users=461843263))
               response2 = conv.wait_event(events.NewMessage(incoming=True,from_users=461843263))
@@ -29,30 +29,30 @@ async def _(W2Hevent):
               response2 = await response2 
               response3 = await response3 
           except YouBlockedUserError: 
-              await W2Hevent.reply("Please unblock ( @Sangmatainfo_bot ) ")
+              await kaalevent.reply("Please unblock ( @Sangmatainfo_bot ) ")
               return
           if response1.text.startswith("No records found"):
-             await eor(W2Hevent, "User never changed his Username...")
+             await eor(kaalevent, "User never changed his Username...")
           else: 
-             await W2Hevent.delete()
-             await W2Hevent.client.send_message(W2Hevent.chat_id, response2.message)
+             await kaalevent.delete()
+             await kaalevent.client.send_message(W2Hevent.chat_id, response2.message)
 
-@W2HBOT.on(admin_cmd(pattern="unh ?(.*)"))
-@W2HBOT.on(sudo_cmd(pattern="unh ?(.*)", allow_sudo=True))
-async def _(W2Hevent):
-    if W2Hevent.fwd_from:
+@kaalBOT.on(admin_cmd(pattern="unh ?(.*)"))
+@kaalBOT.on(sudo_cmd(pattern="unh ?(.*)", allow_sudo=True))
+async def _(kaalevent):
+    if kaalevent.fwd_from:
         return 
-    if not W2Hevent.reply_to_msg_id:
-       await eor(W2Hevent, "`Please Reply To A User To Get This Module Work`")
+    if not kaalevent.reply_to_msg_id:
+       await eor(kaalevent, "`Please Reply To A User To Get This Module Work`")
        return
-    reply_message = await W2Hevent.get_reply_message() 
+    reply_message = await kaalevent.get_reply_message() 
     chat = "Sangmatainfo_bot"
     victim = reply_message.sender.id
     if reply_message.sender.bot:
-       await eor(W2Hevent, "Need actual users. Not Bots")
+       await eor(kaalevent, "Need actual users. Not Bots")
        return
-    await eor(W2Hevent, "Checking...")
-    async with W2Hevent.client.conversation(chat) as conv:
+    await eor(kaalevent, "Checking...")
+    async with kaalevent.client.conversation(chat) as conv:
           try:     
               response1 = conv.wait_event(events.NewMessage(incoming=True,from_users=461843263))
               response2 = conv.wait_event(events.NewMessage(incoming=True,from_users=461843263))
@@ -62,13 +62,13 @@ async def _(W2Hevent):
               response2 = await response2 
               response3 = await response3 
           except YouBlockedUserError: 
-              await W2Hevent.reply("Please unblock ( @Sangmatainfo_bot ) ")
+              await kaalevent.reply("Please unblock ( @Sangmatainfo_bot ) ")
               return
           if response1.text.startswith("No records found"):
-             await eor(W2Hevent, "User never changed his Username...")
+             await eor(kaalevent, "User never changed his Username...")
           else: 
-             await W2Hevent.delete()
-             await W2Hevent.client.send_message(W2Hevent.chat_id, response3.message)
+             await kaalevent.delete()
+             await kaalevent.client.send_message(W2Hevent.chat_id, response3.message)
 
 CmdHelp("history").add_command(
   "history", "<reply to a user>", "Fetches the name history of replied user."
