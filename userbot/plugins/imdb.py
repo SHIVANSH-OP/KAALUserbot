500eb06153337263958b9c3c15cf520154a5c04e
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-05-23 22:43:57 +0530

    Update imdb.py

--- userbot/plugins/imdb.py
+++ userbot/plugins/imdb.py
@@ -10,7 +10,7 @@ import re
 
 import bs4
 import requests
-from W2HBOT.utils import admin_cmd, sudo_cmd, edit_or_reply
+from kaalBOT.utils import admin_cmd, sudo_cmd, edit_or_reply
 from userbot.cmdhelp import CmdHelp
 
 langi = "en"
