da8da2cd8b0904a3a19a85a4522527e4b5ff5b52
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-05-25 23:54:35 +0530

    Update warnbun.py

--- userbot/plugins/warnbun.py
+++ userbot/plugins/warnbun.py
@@ -1,7 +1,7 @@
 """.admin Plugin for @UniBorg"""
 from telethon.tl.types import ChannelParticipantsAdmins
 
-from W2HBOT.utils import admin_cmd, sudo_cmd, edit_or_reply
+from kaalBOT.utils import admin_cmd, sudo_cmd, edit_or_reply
 from userbot.cmdhelp import CmdHelp
 
 
