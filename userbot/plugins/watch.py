e0c8424bd8daad9bb9fed8427d1545871bff0eaf
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-05-25 23:53:15 +0530

    Update watch.py

--- userbot/plugins/watch.py
+++ userbot/plugins/watch.py
@@ -7,7 +7,7 @@
 
 from justwatch import JustWatch
 
-from W2HBOT.utils import admin_cmd, sudo_cmd, edit_or_reply
+from kaalBOT.utils import admin_cmd, sudo_cmd, edit_or_reply
 from userbot.cmdhelp import CmdHelp
 
 
