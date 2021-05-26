e2b4ba347223662c46a0bfb26b029ad9d46d4728
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-05-26 06:50:59 +0530

    Update updater.py

--- userbot/plugins/updater.py
+++ userbot/plugins/updater.py
@@ -5,7 +5,7 @@ import sys
 import git
 
 from userbot.Config import Config
-from W2HBOT.utils import admin_cmd
+from kaalBOT.utils import admin_cmd
 
 # -- Constants -- #
 IS_SELECTED_DIFFERENT_BRANCH = (
@@ -15,13 +15,13 @@ IS_SELECTED_DIFFERENT_BRANCH = (
     "please check out to an official branch, and re-start the updater."
 )
 OFFICIAL_UPSTREAM_REPO = Config.UPSTREAM_REPO
-BOT_IS_UP_TO_DATE = "**The W2HBOT** is up-to-date sir."
+BOT_IS_UP_TO_DATE = "**The kaalBOT** is up-to-date sir."
 NEW_BOT_UP_DATE_FOUND = (
     "new update found for {branch_name}\n"
     "changelog: \n\n{changelog}\n"
-    "updating your W2HBOT ..."
+    "updating your kaalBOT ..."
 )
-NEW_UP_DATE_FOUND = "New update found for {branch_name}\n" "`updating your W2HBOT...`"
+NEW_UP_DATE_FOUND = "New update found for {branch_name}\n" "`updating your kaalBOT...`"
 REPO_REMOTE_NAME = "temponame"
 IFFUCI_ACTIVE_BRANCH_NAME = "master"
 DIFF_MARKER = "HEAD..{remote_name}/{branch_name}"
