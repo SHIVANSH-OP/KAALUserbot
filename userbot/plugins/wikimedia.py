faf44cc745bd4c2239c15ad2eaaf11715d179cf6
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-05-26 07:20:06 +0530

    Update wikimedia.py

--- userbot/plugins/wikimedia.py
+++ userbot/plugins/wikimedia.py
@@ -1,7 +1,7 @@
 """WikiMedia.ORG
 Syntax: .wikimedia Query"""
 import requests
-from W2HBOT.utils import admin_cmd, sudo_cmd, edit_or_reply
+from kaalBOT.utils import admin_cmd, sudo_cmd, edit_or_reply
 from userbot.cmdhelp import CmdHelp
 
 
