7d9c4c574355efa8f117ed272b1f197bdc8a9eca
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-05-26 07:16:50 +0530

    Update zipfile.py

--- userbot/plugins/zipfile.py
+++ userbot/plugins/zipfile.py
@@ -3,7 +3,7 @@ import os
 import time
 import zipfile
 
-from W2HBOT.utils import admin_cmd, sudo_cmd, edit_or_reply
+from kaalBOT.utils import admin_cmd, sudo_cmd, edit_or_reply
 
 
 @bot.on(admin_cmd(pattern="compress ?(.*)", outgoing=True))
