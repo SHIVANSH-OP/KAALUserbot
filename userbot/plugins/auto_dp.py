0ae5c73f5deaf0def6166703f0c76cd434e738e2
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-05-28 17:48:08 +0530

    Update auto_dp.py

--- userbot/plugins/auto_dp.py
+++ userbot/plugins/auto_dp.py
@@ -1,4 +1,4 @@
-# Made By @Murat_30_kaal Keep Credits If You Are Goanna Kang This Lol
+# Made By @Murat_30_God Keep Credits If You Are Goanna Kang This Lol
 
 # And Thanks To The Creator Of Autopic This Script Was Made from Snippets From That Script
 
