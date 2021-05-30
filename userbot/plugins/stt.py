ebf8db6b9515cf277044663d915af6d181792774
kaal0408 <74854072+kaal0408@users.noreply.github.com>
2021-05-30 17:28:02 +0530

    Update stt.py

--- userbot/plugins/stt.py
+++ userbot/plugins/stt.py
@@ -16,7 +16,7 @@ async def _(event):
     input_str = event.pattern_match.group(1)
     if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
         os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
-    W2Hevent = await edit_or_reply(event, "Downloading to my local, for analysis  🙇")
+    kaalevent = await edit_or_reply(event, "Downloading to my local, for analysis  🙇")
     if event.reply_to_msg_id:
         previous_message = await event.get_reply_message()
         required_file_name = await event.client.download_media(
